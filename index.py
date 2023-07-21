8ee7f3be43fcd8f9974bb57daada0fdf0034b437
Jesus Flores <jesus.flores.angulo@gmail.com>
2023-07-21 12:30:32 -0500

    Se termino de pulir la interfaz y se orgnaizo el codigo

--- index.py
+++ index.py
@@ -1,22 +1,26 @@
 from tkinter import ttk
 from tkinter import * 
-import tkinter.font as font
 
 class Product:
 
     def __init__(self, window):
-
+        
+        #+++++++++++++++++++
         # Windows Attributes
+        #+++++++++++++++++++
+
         self.wind = window
         self.wind.title('Calculator')
         self.wind.geometry("860x620")
-        #self.wind.geometry("455x460")
         self.wind.resizable(False, False)
 
+
+        #++++++++++
         # VARIABLES
+        #++++++++++
+
         self.YesPr = False
         self.NoPr = True
-        self.myFont = font.Font(size=30)
         self.operation = []
         self.old_operation = []
         self.TkStyle = ttk.Style()
@@ -24,8 +28,11 @@ class Product:
         self.ResPT = 0
         self.ResPI = 0
         self.ResD = 0
+
         
+        #+++++++++++
         # SCROLLBARS
+        #+++++++++++
 
         #Treeview Scrollbar
         self.barO = Scrollbar(self.wind, orient='horizontal', width=30)
@@ -35,12 +42,15 @@ class Product:
         self.barT = Scrollbar(self.wind, orient='horizontal', width=30)
         self.barT.grid(row=5, column=0, sticky = W + E)
 
+
+        #+++++++++
         # TREEVIEW
+        #+++++++++
+
         self.tree = ttk.Treeview(height=10, columns=2, yscrollcommand=self.barO.set)
         self.tree.grid(row=0, column=0, sticky = E + E)
         self.TkStyle.configure('Treeview', font=('courier 20'))
         self.TkStyle.configure('Treeview.Heading', font=('P052 15'))
-        print(self.TkStyle.layout("Treeview.Heading"))
 
         #Operation Colum
         self.tree.heading('#0', text = 'OPERATIONS', anchor = CENTER)
@@ -50,17 +60,27 @@ class Product:
         self.tree.heading('#1', text = 'RESULT', anchor = CENTER)
         self.tree.column('#1', width=430, stretch=False)
 
-        # Entry
+
+        #++++++
+        # ENTRY
+        #++++++
+
         self.EntryOps = Entry(self.wind, state='readonly', xscrollcommand=self.barT.set, font=('P052 35'), width=37 , textvariable = self.operation)
         self.EntryOps.grid(row=4, column=0)
+
 
         # Scrollbars Configuration
         self.barT.configure(command=self.EntryOps.xview) 
         self.barO.configure(command=self.tree.yview)
         
-        # OPERATIONS BUTTONS
 
+        #+++++++++++++++++++
+        # OPERATIONS BUTTONS
+        #+++++++++++++++++++
+        
+        #++++++++++++++++
         # NUMBERS BUTTONS
+        #++++++++++++++++
 
         #Button nine
         self.nine = ttk.Button(text='9', width=20, command = lambda: self.printOps(9))
@@ -102,53 +122,59 @@ class Product:
         self.zero = ttk.Button(text='0', width=20, command = lambda: self.printOps(0))
         self.zero.grid(row = 8, column = 0, sticky=S + S, padx=[0,690], ipady=10)
 
-        # SYMBOLS BUTTONS
 
-        #EQUAL
+        #++++++++++++++++
+        # SYMBOLS BUTTONS
+        #++++++++++++++++
+
+        #Equal
         self.equal = ttk.Button(text='=', width=21, command = lambda: self.printRes())
         self.equal.grid(row = 8, column = 0, sticky=E + E, padx=[0,0], ipady=35)
 
-        #DEL ALL
+        #Del All
         self.delAll = ttk.Button(text='DEL ALL', width=55, command = lambda: self.delAEnt())
         self.delAll.grid(row = 9, column = 0, sticky=W + W, padx=[0,0], ipady=10)
 
-        #DEL
+        #Del
         self.delBtn = ttk.Button(text='DEL', width=55, command = lambda: self.delEnt())
         self.delBtn.grid(row = 9, column = 0, sticky=E + E, padx=[0,0], ipady=10)
 
-        #PLUS
+        #Plus
         self.plus = ttk.Button(text='+', width=20, command = lambda: self.printOps('+'))
         self.plus.grid(row = 8, column = 0, sticky=S + S, padx=[338,0], ipady=10)
 
-        #MINUS
+        #Minus
         self.minus = ttk.Button(text='-', width=20, command = lambda: self.printOps('-'))
         self.minus.grid(row = 8, column = 0, sticky=N + N, padx=[338,0], ipady=10)
 
-        #MULTI
+        #Multi
         self.multi = ttk.Button(text='x', width=20, command = lambda: self.printOps('*'))
         self.multi.grid(row = 6, column = 0, sticky=E + E, padx=[0,177], ipady=10)
 
-        #DIVISION
+        #Division
         self.division = ttk.Button(text='÷', width=20, command = lambda: self.printOps('/'))
         self.division.grid(row = 7, column = 0, sticky=E + E, padx=[0,177], ipady=10)
 
-        #PNTSR
+        #pntsr
         self.pntsr = ttk.Button(text='(', width=20, command = lambda: self.printOps('('))
         self.pntsr.grid(row = 6, column = 0, sticky=E + E, padx=[0,5], ipady=10)
         
-        #PNTSL
+        #pntsl
         self.pntsl = ttk.Button(text=')', width=20, command = lambda: self.printOps(')'))
         self.pntsl.grid(row = 7, column = 0, sticky=E + E, padx=[0,5], ipady=10)
 
-        #COMMA
+        #Comma
         self.comma = ttk.Button(text=',', width=20, command = lambda: self.printOps(','))
         self.comma.grid(row = 8, column = 0, sticky=S + S,  padx=[0,350], ipady=10)
 
-        #PERCENT
-        self.percent = Button(text='%', width=20, command = lambda: self.printOps('%'))
+        #Percent
+        self.percent = ttk.Button(text='%', width=20, command = lambda: self.printOps('%'))
         self.percent.grid(row = 8, column = 0, sticky=S + S, padx=[0,7], ipady=10)
 
-    # Functions
+
+    #+++++++++
+    # FUNCTIONS
+    #+++++++++
 
     #Function to print the operation in the Entry
     def printOps(self, number):
@@ -189,7 +215,7 @@ class Product:
         self.NoPr = True
 
 
-    # Print the result into the Entry and the TreeView
+    #Function to print the result into the Entry and the TreeView
     def printRes(self):
         if self.YesPr == True:
             try: 
@@ -208,9 +234,9 @@ class Product:
             except ZeroDivisionError:
                 self.EntryOps.configure(state='normal')
                 self.EntryOps.delete(0, END)
-                self.EntryOps.insert(0, 'Presione DEL para continuar')
+                self.EntryOps.insert(0, 'Error: Press DEL ALL to continue')
                 self.operation = self.ResPT
-                self.tree.insert('', 0, text = self.operation, values = ["Error: Division por cero"])
+                self.tree.insert('', 0, text = self.operation, values = ["Error: Division by zero"])
                 self.operation = []
                 self.YesPr = False
                 self.NoPr = False
@@ -219,15 +245,13 @@ class Product:
             except SyntaxError:
                 self.EntryOps.configure(state='normal')
                 self.EntryOps.delete(0, END)
-                self.EntryOps.insert(0, 'Presione DEL para continuar')
+                self.EntryOps.insert(0, 'Error: Press DEL ALL to continue')
                 self.operation = self.ResPT
-                self.tree.insert('', 0, text = self.operation, values = ["Error: Operacion invalida"])
+                self.tree.insert('', 0, text = self.operation, values = ["Error: Invalid Operation"])
                 self.operation = []
                 self.YesPr = False
                 self.NoPr = False
                 self.EntryOps.configure(state='readonly')
-
-
 
 if __name__ == '__main__':
     window = Tk()
